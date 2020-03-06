d907c37aa13f10f969dd062542a432577e8d6a76
Arvind Nedumaran <arvindamirtaa@gmail.com>
2020-03-07 02:04:38 +0530

    Added OrgProfile, Requests & Requirements models

--- organisations/models.py
+++ organisations/models.py
@@ -1,21 +1,40 @@
 from django.db import models
 
+from .vars import ORG_TYPE_CHOICES
 
 # Create your models here.
-class Organisation(models.Model):
+
+
+class OrganisationProfile(models.Model):
+    # TODO Add FK to User model.
     name = models.CharField(max_length=200)
-    website = models.URLField()
     description = models.TextField()
-    name_of_contact = models.CharField(max_length=200)
-    email = models.EmailField()
-    phone = models.CharField(max_length=200)
+    website = models.URLField()
+    category = models.IntegerField(choices=ORG_TYPE_CHOICES)
+    address = models.CharField(max_length=200)
+    # TODO Use django-cities to select City from list.
     city = models.CharField(max_length=40)
+    contact_name = models.CharField(max_length=200)
+    contact_email = models.EmailField()
+    contact_phone = models.CharField(max_length=200)
 
 
-class AskingOrg(Organisation):
-    work_description = models.TextField()
-    requirements = models.TextField()
+class Requirement(models.Model):
+    organisation = models.ForeignKey(
+        OrganisationProfile, related_name='requirements')
+    category = models.IntegerField(choices=CATEGORY_CHOICES)
+    what = models.TextField()
+    why = models.TextField()
+    # TODO Configure static & media files and change to FileField.
+    supporting_document = models.URLField()
 
 
-class HelpingOrg(Organisation):
-    resource_description = models.TextField()
+class Resource(models.Model):
+    organisation = models.ForeignKey(
+        OrganisationProfile, related_name='resources')
+    category = models.IntegerField(choices=CATEGORY_CHOICES)
+    description = models.TextField()
+    availability = models.TextField()
+    eligibility = models.TextField()
+    # TODO Configure static & media files and change to FileField.
+    supporting_document = models.URLField()
